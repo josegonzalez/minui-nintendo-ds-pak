9cd9ffaa14b8e72433452519dd215fb785c36f0c
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2025-03-28 12:05:18 -0400

    refactor: cleanup launch.sh
    - ship logs to correct path
    - remove extra random logging
    - set and cleanup the cpu settings in the same launch.sh
    - drop the code that cleared the recents list
    - use a single launch.sh file

--- launch.sh
+++ launch.sh
@@ -1,22 +1,47 @@
 #!/bin/sh
+set -eo pipefail
+set -x
+
+rm -f "$LOGS_PATH/NDS.txt"
+exec >>"$LOGS_PATH/NDS.txt"
+exec 2>&1
+
 echo $0 $*
-progdir=`dirname "$0"`/drastic
-cd $progdir
 
+mkdir -p "$USERDATA_PATH/NDS-advanced-drastic"
+EMU_DIR="$SDCARD_PATH/Emus/$PLATFORM/NDS.pak/drastic"
+PACK_DIR="$SDCARD_PATH/Emus/$PLATFORM/NDS.pak"
 
+export PATH=$EMU_DIR:$PACK_DIR/bin:$PATH
+export LD_LIBRARY_PATH=$EMU_DIR/libs:$PACK_DIR/lib:$LD_LIBRARY_PATH
 
-echo "=============================================="
-echo "==================== DRASTIC ================="
-echo "=============================================="
+cleanup() {
+    rm -f /tmp/stay_awake
 
-../performance.sh
+    if [ -f "$USERDATA_PATH/NDS-advanced-drastic/cpu_governor.txt" ]; then
+        echo "$(cat "$USERDATA_PATH/NDS-advanced-drastic/cpu_governor.txt")" >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+        rm -f "$USERDATA_PATH/NDS-advanced-drastic/cpu_governor.txt"
+    fi
+    if [ -f "$USERDATA_PATH/NDS-advanced-drastic/cpu_min_freq.txt" ]; then
+        echo "$(cat "$USERDATA_PATH/NDS-advanced-drastic/cpu_min_freq.txt")" >/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+        rm -f "$USERDATA_PATH/NDS-advanced-drastic/cpu_min_freq.txt"
+    fi
+}
 
-export HOME="$progdir"
-#export SDL_AUDIODRIVER=dsp
-./launch.sh "$*"
+main() {
+    echo "1" >/tmp/stay_awake
+    trap "cleanup" EXIT INT TERM HUP QUIT
 
+    cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor >"$USERDATA_PATH/NDS-advanced-drastic/cpu_governor.txt"
+    cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq >"$USERDATA_PATH/NDS-advanced-drastic/cpu_min_freq.txt"
 
+    echo performance >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+    echo 1500000 >/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 
+    cd "$EMU_DIR"
+    export HOME="$EMU_DIR"
 
+    "$EMU_DIR/drastic" "$*"
+}
 
-> /mnt/SDCARD/.userdata/shared/.minui/recent.txt
+main "$@"
